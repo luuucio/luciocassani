643820d4247822fa8fb4a6363d3f770205a6d78a
Lucio Cassani <luciocassani@MacBook-Pro-di-Lucio.local>
2019-05-15 22:28:37 +0200

    Before zoom

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import Image from "../resources/mouse.jpg";
+import { Typography } from "@material-ui/core";
 
 const styles = theme => ({
   background: {
@@ -10,12 +11,26 @@ const styles = theme => ({
     backgroundSize: "cover",
     backgroundPosition: "center",
     height: "100vh"
+  },
+  headline: {
+    position: "absolute",
+    top: "70%",
+    width: "100%",
+    left: 0,
+    color: "white",
+    textAlign: "center"
   }
 });
 
 const Home = props => {
   const { classes } = props;
-  return <div className={classes.background} />;
+  return (
+    <div className={classes.background}>
+      <Typography variant="h3" className={classes.headline}>
+        Software developer
+      </Typography>
+    </div>
+  );
 };
 
 export default withStyles(styles)(Home);
