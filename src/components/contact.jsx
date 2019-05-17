96eeeba1250844730c4ed82d49abc1420839696b
Lucio Cassani <luciocassani@MacBook-Pro-di-Lucio.local>
2019-05-17 22:58:18 +0200

    Before adding fontawesome icons

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
-import { Typography } from "@material-ui/core";
+import { Typography, Grid } from "@material-ui/core";
 
 const styles = theme => ({
   background: {
     display: "flex",
     height: "100vh",
     backgroundColor: "rgb(0, 50, 0)",
-    color: "white",
+    color: "white"
+  },
+  gridText: {
+    display: "flex",
     justifyContent: "center",
     alignItems: "center"
+  },
+  gridIcons: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-start"
   }
 });
 
@@ -17,9 +25,23 @@ const Contact = props => {
   const { classes } = props;
   return (
     <div className={classes.background} id="Contact">
-      <Typography variant="h4" color="inherit">
-        Contacts
-      </Typography>
+      <Grid container>
+        <Grid container>
+          <Grid item xs={12} className={classes.gridText}>
+            <Typography variant="h4" color="inherit">
+              Contacts
+            </Typography>
+          </Grid>
+        </Grid>
+
+        <Grid container>
+          <Grid item xs={12} className={classes.gridIcons}>
+            <Typography variant="h4" color="inherit">
+              Contacts
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 };
